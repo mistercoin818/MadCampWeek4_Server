3467774e6bc42ab03d5072241a69aa1844d7fa3d
mistercoin818 <kthnice@g.skku.edu>
2023-07-27 09:04:35 +0000

    prototype

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const cors = require("cors");
 
 const PORT = 80;
 const bodyParser = require('body-parser');
+const { utcToZonedTime } = require('date-fns-tz');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
-const mysql = require('mysql2')
+const mysql = require('mysql2');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
@@ -17,17 +17,15 @@ const connection = mysql.createConnection({
     database: 'week4'
 });
 
-// 모든 라우트에 대해 CORS 활성화
 app.use(cors({
-    origin: 'http://localhost:3000', // Allow requests from this origin
+    origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-// MySQL 서버 연결
 connection.connect((err) => {
     if (err) {
-        console.log(err)
+        console.log(err);
         console.error('MySQL 서버에 접속할 수 없습니다.');
         return;
     }
@@ -35,19 +33,70 @@ connection.connect((err) => {
     console.log('MySQL 서버에 접속되었습니다.');
 });
 
+function toDateTime(dateString) {
+    return new Date(dateTimeString);
+}
+
+// 로그인 라우트를 추가
+app.post('/login', (req, res) => {
+    const { password, username } = req.body;
+    console.log(req.body);
+
+    // 데이터베이스에서 해당 UID와 PW를 가진 사용자 정보를 조회하는 쿼리
+    const loginQuery = `
+        SELECT UID
+        FROM users
+        WHERE PW = ? AND username = ?
+    `;
+
+    runQuery(loginQuery, [password, username])
+        .then(results => {
+            // 조회된 결과가 있으면 로그인 성공, 그렇지 않으면 로그인 실패
+            if (results.length > 0) {
+                const { UID } = results[0];
+                res.status(200).json({ UID });
+            } else {
+                res.status(401).json({ message: '로그인 실패' });
+            }
+        })
+        .catch(err => {
+            console.error('로그인 중 오류가 발생했습니다:', err);
+            res.status(500).json({ error: '로그인 오류' });
+        });
+});
+
+// 회원가입 라우트를 추가
+app.post('/join', (req, res) => {
+    const { PW, username } = req.body;
+
+    // 데이터베이스에 새로운 사용자 정보를 추가하는 쿼리
+    const addUserQuery = `
+        INSERT INTO users (PW, username)
+        VALUES (?, ?)
+    `;
+
+    runQuery(addUserQuery, [PW, username])
+        .then(() => {
+            res.status(200).json({ message: '회원가입 성공' });
+        })
+        .catch(err => {
+            console.error('회원가입 중 오류가 발생했습니다:', err);
+            res.status(500).json({ error: '회원가입 오류' });
+        });
+});
+
 app.get('/dashboard', (req, res) => {
-    const { UID } = req.query; // Frontend에서 UID를 query parameter로 보낼 때 사용합니다.
+    const { UID } = req.query;
 
-    // 매일 시작일과 종료일을 계산
-    const endDate = new Date(); // 오늘 날짜 (기본값)
-    const startDate = new Date(endDate);
-    startDate.setDate(startDate.getDate() - 6); // 7일 전으로 설정
+    const tz = 'Asia/Seoul';
+    const endDate = new Date();
+    const zonedEndDate = utcToZonedTime(endDate, tz);
+    const startDate = new Date(zonedEndDate);
+    startDate.setDate(startDate.getDate() - 6);
 
-    // 날짜를 MySQL에서 사용할 수 있는 문자열로 변환
     const startDateStr = startDate.toISOString().slice(0, 10);
-    const endDateStr = endDate.toISOString().slice(0, 10);
+    const endDateStr = zonedEndDate.toISOString().slice(0, 10);
 
-    // 월별 총 소비량을 계산하는 쿼리
     const monthlyCostQuery = `
         SELECT 
             DATE_FORMAT(date, '%Y-%m') AS month,
@@ -62,7 +111,6 @@ app.get('/dashboard', (req, res) => {
             DATE_FORMAT(date, '%Y-%m')
     `;
 
-    // 이번 달의 1주차부터 4주차까지의 소비량을 계산하는 쿼리
     const weeklyCostQuery = `
         SELECT 
             CASE 
@@ -83,24 +131,6 @@ app.get('/dashboard', (req, res) => {
             week ASC
     `;
 
-    // 이번 달의 1주차부터 4주차까지의 소비량을 계산하는 쿼리
-    // const weeklyCostQuery = `
-    //     SELECT 
-    //         weeks.week_num,
-    //         IFNULL(SUM(books.cost), 0) AS total_cost
-    //     FROM 
-    //         weeks
-    //     LEFT JOIN books ON weeks.week_num = WEEK(DATE_FORMAT(books.date, '%Y-%m-%d'), 1)
-    //                 AND books.UID = ?
-    //                 AND DATE_FORMAT(books.date, '%Y-%m') = DATE_FORMAT(CURDATE(), '%Y-%m')
-    //     GROUP BY 
-    //         weeks.week_num
-    //     ORDER BY 
-    //         weeks.week_num ASC
-    // `;
-
-
-    // 최근 7일 동안의 소비내역을 가져오는 쿼리
     const recentCostQuery = `
         SELECT
             DATE_FORMAT(date, '%Y-%m-%d') AS date,
@@ -115,7 +145,6 @@ app.get('/dashboard', (req, res) => {
             DATE_FORMAT(date, '%Y-%m-%d')
     `;
 
-    // 이번 달의 category별 소비한 금액을 계산하는 쿼리
     const categoryCostQuery = `
         SELECT 
             category,
@@ -130,74 +159,61 @@ app.get('/dashboard', (req, res) => {
             category
     `;
 
-    // Define the desired order of categories
     const desiredOrder = ["식비", "주거", "교통", "쇼핑", "의료", "여가", "기타"];
 
     Promise.all([
         runQuery(monthlyCostQuery, [UID]),
         runQuery(weeklyCostQuery, [UID]),
-        runQuery(recentCostQuery, [UID, startDateStr, endDateStr]), // 수정된 부분
+        runQuery(recentCostQuery, [UID, startDateStr, endDateStr]),
         runQuery(categoryCostQuery, [UID])
     ]).then(results => {
         const [monthlyCost, weeklyCost, recentCost, categoryCost] = results;
 
-        // Prepare an object to store the category names and their total costs
         const categoryCostMap = {};
         categoryCost.forEach(category => {
             categoryCostMap[category.category] = category.total_cost;
         });
 
-        // Fetch the list of all unique categories from the books table
-        const uniqueCategoriesQuery = `
-            SELECT DISTINCT category FROM books WHERE UID = ?
-        `;
-        runQuery(uniqueCategoriesQuery, [UID]).then(uniqueCategories => {
-            // Initialize the responseData object with all categories and set their total costs to 0
-            const responseData = {
-                monthlyCost,
-                weeklyCost,
-                recentCost: [], // Initialize as an empty array
-                categoryCost: {}
-            };
+        const responseData = {
+            monthlyCost,
+            weeklyCost: [0, 0, 0, 0], // Initialize all weeks with 0
+            recentCost: [],
+            categoryCost: {}
+        };
 
-            // Populate the responseData.categoryCost object in the desired order
-            desiredOrder.forEach(category => {
-                responseData.categoryCost[category] = categoryCostMap[category] || 0;
-            });
-
-            // Fetch the list of all dates within the recent 7-day period
-            const recentDates = [];
-            const currentDate = new Date(endDate);
-            for (let i = 0; i < 7; i++) {
-                recentDates.unshift(currentDate.toISOString().slice(0, 10));
-                currentDate.setDate(currentDate.getDate() - 1);
-            }
-
-            // Prepare an object to store the recent dates and their total costs
-            const recentCostMap = {};
-            recentDates.forEach(date => {
-                recentCostMap[date] = 0;
-            });
-
-            // Populate the recentCostMap with actual data from the database
-            recentCost.forEach(item => {
-                recentCostMap[item.date] = item.total_cost;
-            });
-
-            // Populate the responseData.recentCost array with recent date data
-            recentDates.forEach(date => {
-                responseData.recentCost.push({
-                    date: date,
-                    total_cost: recentCostMap[date]
-                });
-            });
-
-            // 결과를 JSON 형식으로 응답
-            res.json(responseData);
-        }).catch(err => {
-            console.error('쿼리를 실행하는데 오류가 발생했습니다.', err);
-            res.status(500).json({ error: '쿼리 실행 오류' });
+        desiredOrder.forEach(category => {
+            responseData.categoryCost[category] = categoryCostMap[category] || 0;
         });
+
+        const recentDates = [];
+        const currentDate = new Date(zonedEndDate);
+        for (let i = 0; i < 7; i++) {
+            recentDates.unshift(currentDate.toISOString().slice(0, 10));
+            currentDate.setDate(currentDate.getDate() - 1);
+        }
+
+        const recentCostMap = {};
+        recentDates.forEach(date => {
+            recentCostMap[date] = 0;
+        });
+
+        recentCost.forEach(item => {
+            recentCostMap[item.date] = item.total_cost;
+        });
+
+        recentDates.forEach(date => {
+            responseData.recentCost.push({
+                date: date,
+                total_cost: recentCostMap[date]
+            });
+        });
+
+        weeklyCost.forEach(item => {
+            responseData.weeklyCost[item.week - 1] = item.total_cost;
+        });
+
+        console.log(responseData);
+        res.json(responseData);
     }).catch(err => {
         console.error('쿼리를 실행하는데 오류가 발생했습니다.', err);
         res.status(500).json({ error: '쿼리 실행 오류' });
@@ -208,7 +224,6 @@ app.get('/calendar/get', (req, res) => {
     console.log(req.query);
     const { UID, day } = req.query;
 
-    // Query to fetch the expense data for the given UID and date
     const getExpenseQuery = `
         SELECT *
         FROM books
@@ -218,7 +233,7 @@ app.get('/calendar/get', (req, res) => {
     runQuery(getExpenseQuery, [UID, day])
         .then(results => {
             res.status(200).json(results);
-            console.log(results)
+            console.log(results);
         })
         .catch(err => {
             console.error('Error fetching expense data:', err);
@@ -226,15 +241,11 @@ app.get('/calendar/get', (req, res) => {
         });
 });
 
-
 app.post('/calendar/post', (req, res) => {
     console.log(req.body);
     const { UID, date, time, detail, cost, category } = req.body;
-
-    // Combine date and time to create a single datetime value
     const datetime = new Date(date + ' ' + time);
 
-    // Insert the new expense into the 'books' table
     const addExpenseQuery = `
         INSERT INTO books (UID, date, detail, cost, category)
         VALUES (?, ?, ?, ?, ?)
@@ -250,8 +261,26 @@ app.post('/calendar/post', (req, res) => {
         });
 });
 
+app.delete('/calendar/delete', (req, res) => {
+    const { HID, UID } = req.body;
 
-// 쿼리 실행 함수
+    // Query to delete the expense data from the 'books' table
+    const deleteExpenseQuery = `
+        DELETE FROM books
+        WHERE HID = ? AND UID = ?
+    `;
+
+    runQuery(deleteExpenseQuery, [HID, UID])
+        .then(() => {
+            res.status(200).json({ message: 'Expense deleted successfully.' });
+        })
+        .catch(err => {
+            console.error('Error deleting expense:', err);
+            res.status(500).json({ error: 'Failed to delete expense.' });
+        });
+});
+
+
 function runQuery(query, params) {
     return new Promise((resolve, reject) => {
         connection.query(query, params, (err, results) => {
